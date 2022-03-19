56e2b661a923e964aa9816a041c5639110e5e154
SeonjuKim <o6o4_@naver.com>
2022-03-19 10:58:09 +0900

    Refactor(Seonju/Graph Traversal): 백준 2636 치즈

--- Seonju/Baekjoon/Graph Traversal/2636_치즈.py
+++ Seonju/Baekjoon/Graph Traversal/2636_치즈.py
@@ -5,65 +5,42 @@ from collections import deque
 input = sys.stdin.readline
 
 
-n, m = map(int, input().split())
-cheeze = []
-cnt = 0
-for i in range(n):
-    cheeze.append(list(map(int, input().split())))
-    cnt += cheeze[i].count(1)  # 전체 치즈 갯수 카운팅
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def check_air(x, y):  # 공기 배열 air 초기화 (공기인 곳은 값을 1로)
-    q = deque([(x, y)])
-    air = [[0] * m for _ in range(n)]
-    air[x][y] = 1
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and not cheeze[nx][ny] and not air[nx][ny]:
-                air[nx][ny] = 1
-                q.append((nx, ny))
-    return air
-
-
-def melt_cheeze(x, y):  # 공기와 맞닿은 치즈 녹이기
-    q = deque([(x, y)])
-    melt_cnt = 0
+def bfs():
+    q = deque([(0, 0)])
+    melt = deque([])
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-                if air[nx][ny] and cheeze[x][y]:
-                    cheeze[x][y] = 0
-                    air[x][y] = 1
-                    melt_cnt += 1
-                elif cheeze[nx][ny] and not visited[nx][ny]:
+                visited[nx][ny] = 1
+                if cheeze[nx][ny] == 0:  # 공기면 계속 탐색하기 위해 큐에 넣음
                     q.append((nx, ny))
-                    visited[nx][ny] = 1
-    return melt_cnt
+                elif cheeze[nx][ny] == 1:  # 치즈면 한 번에 녹이기 위해 멜트큐에 넣음
+                    melt.append((nx, ny))
 
+    for x, y in melt:
+        cheeze[x][y] = 0  # 공기와 닿은 치즈를 한 번에 녹임
+
+    return len(melt)  # 녹인 치즈 갯수 리턴
+
+
+n, m = map(int, input().split())
+cheeze = []
+cnt = 0
+for i in range(n):
+    cheeze.append(list(map(int, input().split())))
+    cnt += sum(cheeze[i])  # 전체 치즈 갯수
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 time = 1
 while True:
-    now_melt = 0
     visited = [[0] * m for _ in range(n)]
-
-    air = check_air(0, 0)
-
-    for i in range(n):
-        for j in range(m):
-            if cheeze[i][j] and not visited[i][j]:
-                now_melt += melt_cheeze(i, j)
-    cnt -= now_melt
-
+    meltCnt = bfs()
+    cnt -= meltCnt
     if cnt == 0:
-        print(time, now_melt, sep='\n')
+        print(time, meltCnt, sep='\n')
         break
-
     time += 1
