b055415376641159e53505d0b2376f0f3d1a879e
Soomin Ryu <rsm0932@gmail.com>
2022-03-24 15:15:41 +0000

    Add(Soomin/Graph Traversal): 백준 14248 점프 점프

---
+++ Soomin/Baekjoon/Graph Traversal/14248 점프 점프.py
@@ -0,0 +1,34 @@
+#20220325
+#백준(그래프 탐색) : 14248 점프 점프
+#https://www.acmicpc.net/problem/14248
+
+from collections import deque
+
+def bfs(v):
+  q = deque()
+  q.append(v)
+
+  visited[v] = 1
+
+  while q:
+    a = q.popleft()
+
+    for i in [-rock[a], rock[a]]:
+      jump = a + i
+
+      if 0 <= jump < n and visited[jump] == 0:
+        q.append(jump)
+        visited[jump] = 1
+    
+
+
+n = int(input())
+
+rock = list(map(int,input().split()))
+print(rock)
+s = int(input())
+
+visited = [0] * n
+bfs(s-1)
+
+print(visited.count(1))
