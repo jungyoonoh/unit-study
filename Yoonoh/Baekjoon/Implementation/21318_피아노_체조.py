efbcea5a62d87cd8b315c730619f843b0049164e
Yoonoh <xkakak142@naver.com>
2022-04-04 13:14:03 +0900

    Add(Yoonoh/Implementation): 백준 21318 피아노 체조

---
+++ Yoonoh/Baekjoon/Implementation/21318_피아노_체조.py
@@ -0,0 +1,16 @@
+# 21318번 피아노 체조 (Implementation) 
+# https://www.acmicpc.net/problem/21318
+
+N = int(input())
+level = list(map(int, input().split()))
+miss = [0] * N
+for i in range(1, N):
+    if level[i-1] > level[i]:
+        miss[i] = miss[i-1] + 1 # 실수
+    else:
+        miss[i] = miss[i-1]
+
+Q = int(input())
+for _ in range(Q):
+    start, end = map(int, input().split())
+    print(miss[end-1] - miss[start-1])
