ca0d5b4c106485bfbaa4098c734c072da379ce21
Yoonoh <xkakak142@naver.com>
2022-04-26 12:26:59 +0900

    Add(Yoonoh/BruteForce): 백준 15664 N과 M 10

---
+++ Yoonoh/Baekjoon/BruteForce/15664_N과_M_10.py
@@ -0,0 +1,29 @@
+# 15664번 N과 M (10) (Graph) 
+# https://www.acmicpc.net/problem/15664
+
+N, M = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+answerPool = set()
+def getSequence(seq, idx, cnt, isSelected):
+    
+    if cnt == M:
+        if len(seq) == 1:
+            if seq[0] not in answerPool:
+                print(*seq)
+                answerPool.add(seq[0])
+        else:
+            if ''.join(seq) not in answerPool:            
+                print(*seq)
+                answerPool.add(''.join(seq))
+        return        
+        
+    for i in range(idx, N):
+        if not isSelected[i]:
+            isSelected[i] = True
+            seq.append(str(data[i]))
+            getSequence(seq, i, cnt + 1, isSelected)
+            seq.pop()
+            isSelected[i] = False
+    
+getSequence([], 0, 0, [False for _ in range(N)])
