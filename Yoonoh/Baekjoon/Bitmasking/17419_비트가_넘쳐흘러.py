fe28b0b904144251026e99c449bbfdf442439e6d
Yoonoh <xkakak142@naver.com>
2022-06-03 11:34:30 +0900

    Add(Yoonoh/Bitmasking): 백준 17419 비트가 넘쳐흘러

---
+++ Yoonoh/Baekjoon/Bitmasking/17419_비트가_넘쳐흘러.py
@@ -0,0 +1,12 @@
+# 17419번 비트가 넘쳐흘러 (Bitmasking) 
+# https://www.acmicpc.net/problem/17419
+
+N = int(input())
+print(input().count("1"))
+
+# K = int(input(), 2)
+# cnt = 0
+# while K != 0:
+#     K = K-(K&((~K)+1))
+#     cnt += 1
+# print(cnt)
